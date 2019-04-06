a4130403c9e3a0e3e7af08269296c71c6b9945bf
Kyle Robert <notoriouskro@gmail.com>
2019-04-05 20:30:41 -0700

    connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgerName) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burgerName) VALUES ('Smokehouse Burger');
+INSERT INTO burgers (burgerName) VALUES ('Bacon Avocado Burger');
